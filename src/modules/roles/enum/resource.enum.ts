0af2fdd033acb554448cd171dc4d37a208e37ec7
vietnhan1342002 <cudum13402nhan@gmail.com>
2024-11-01 15:03:31 +0700

    add resource ALL for admin

--- src/modules/roles/enum/resource.enum.ts
+++ src/modules/roles/enum/resource.enum.ts
@@ -1,11 +1,12 @@
 export enum Resource {
-    USER = 'user',
-    DOCTOR = 'doctor',
-    PATIENT = 'patient',
-    RECEPTIONIST = 'receptionist',
-    APPOINTMENT = 'appointment',
-    MEDICAL_RECORD = 'medical_record',
-    DEPARTMENT = 'department',
-    BILLING = 'billing',
-    REPORT = 'report',
-  }
+  ALL = 'all',
+  USER = 'user',
+  DOCTOR = 'doctor',
+  PATIENT = 'patient',
+  RECEPTIONIST = 'receptionist',
+  APPOINTMENT = 'appointment',
+  MEDICAL_RECORD = 'medical_record',
+  DEPARTMENT = 'department',
+  BILLING = 'billing',
+  REPORT = 'report',
+}
