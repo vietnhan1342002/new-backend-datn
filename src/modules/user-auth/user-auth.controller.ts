8e91815f702ffa4158d6f8b5d8067bfe3d346e85
vietnhan1342002 <cudum13402nhan@gmail.com>
2024-11-01 11:02:00 +0700

    findAll User

--- src/modules/user-auth/user-auth.controller.ts
+++ src/modules/user-auth/user-auth.controller.ts
@@ -5,6 +5,8 @@ import {
   Body,
   UseGuards,
   Request,
+  Query,
+  Param,
 } from '@nestjs/common';
 import { UserAuthService } from './user-auth.service';
 import { CreateUserAuthDto } from './dto/create-user-auth.dto';
@@ -19,17 +21,25 @@ import { Permissions } from '@/decorator/permission.decorator';
 export class UserAuthController {
   constructor(private readonly userAuthService: UserAuthService) {}
 
+  @Get()
+  async findAll(
+    @Query() query: string,
+    @Param('current') current: string,
+    @Param('pageSize') pageSize: string,
+  ) {
+    return this.userAuthService.findAll(query, +current, +pageSize);
+  }
+  @UseGuards(LocalStrategy)
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+    return this.userAuthService.login(loginDto);
+  }
+
   @UseGuards(JwtAuthGuard)
   @Permissions([{ resource: Resource.USER, actions: [Action.all] }])
   @Post()
   create(@Body() createUserAuthDto: CreateUserAuthDto) {
     return this.userAuthService.create(createUserAuthDto);
-  }
-
-  @UseGuards(LocalStrategy)
-  @Post('login')
-  async login(@Body() loginDto: LoginDto) {
-    return this.userAuthService.login(loginDto);
   }
 
   @UseGuards(JwtAuthGuard)
