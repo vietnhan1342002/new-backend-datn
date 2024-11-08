edfeb76d8466606d4b36ec0afd0746eba87bfb73
vietnhan1342002 <cudum13402nhan@gmail.com>
2024-11-08 14:05:03 +0700

    delete modules unnecessary

--- src/modules/schedule_status/dto/update-schedule_status.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateScheduleStatusDto } from './create-schedule_status.dto';
-
-export class UpdateScheduleStatusDto extends PartialType(CreateScheduleStatusDto) {}
