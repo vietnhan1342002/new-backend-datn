af57b3adb6f7cad266498380fba04c61dd7ef3c7
vietnhan1342002 <cudum13402nhan@gmail.com>
2024-11-08 14:05:03 +0700

    delete modules unnecessary

--- src/modules/appointment_status/dto/update-appointment_status.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAppointmentStatusDto } from './create-appointment_status.dto';
-
-export class UpdateAppointmentStatusDto extends PartialType(CreateAppointmentStatusDto) {}
--- src/modules/appointmets/dto/update-appointmet.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAppointmetDto } from './create-appointmet.dto';
-
-export class UpdateAppointmetDto extends PartialType(CreateAppointmetDto) {}
