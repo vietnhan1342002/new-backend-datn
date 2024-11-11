4ff186413f27c17933b97068e7bdf2d2a8432031
vietnhan1342002 <cudum13402nhan@gmail.com>
2024-11-11 14:08:00 +0700

    function create specialty

--- src/modules/specialties/dto/create-specialty.dto.ts
+++ src/modules/specialties/dto/create-specialty.dto.ts
@@ -1,1 +1,9 @@
-export class CreateSpecialtyDto {}
+import { IsNotEmpty } from 'class-validator';
+
+export class CreateSpecialtyDto {
+  @IsNotEmpty({ message: 'name không được để trống' })
+  name: string;
+
+  @IsNotEmpty({ message: 'description không được để trống' })
+  description: string;
+}
