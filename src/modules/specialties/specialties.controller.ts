41dbffde7b508d25d6f941922d27bde6644f1fc4
vietnhan1342002 <cudum13402nhan@gmail.com>
2024-11-11 14:41:43 +0700

    done base CRUD specialty

--- src/modules/specialties/specialties.controller.ts
+++ src/modules/specialties/specialties.controller.ts
@@ -26,40 +26,43 @@ export class SpecialtiesController {
     return this.specialtiesService.create(createSpecialtyDto);
   }
 
-  // @Get()
-  // async findAll(
-  //   @Query('query') query: string = '', // Sử dụng giá trị mặc định là chuỗi rỗng nếu không có query
-  //   @Query('current') current: string = '1',
-  //   @Query('pageSize') pageSize: string = '10',
-  // ) {
-  //   const currentPage = this.parseQueryParam(current);
-  //   const pageLimit = this.parseQueryParam(pageSize);
+  @Get()
+  async findAll(
+    @Query('query') query: string = '', // Sử dụng giá trị mặc định là chuỗi rỗng nếu không có query
+    @Query('current') current: string = '1',
+    @Query('pageSize') pageSize: string = '10',
+  ) {
+    const currentPage = this.parseQueryParam(current);
+    const pageLimit = this.parseQueryParam(pageSize);
 
-  //   // Tìm tất cả specialties hoặc theo query
-  //   return this.specialtiesService.findAll(query, currentPage, pageLimit);
-  // }
+    // Tìm tất cả bác sĩ hoặc theo query
+    return this.specialtiesService.findAll(query, currentPage, pageLimit);
+  }
 
-  // // Hàm phụ trợ để kiểm tra và phân tích các tham số
-  // private parseQueryParam(value: string): number {
-  //   const parsedValue = parseInt(value, 10);
-  //   if (isNaN(parsedValue) || parsedValue < 1) {
-  //     throw new BadRequestException('Invalid parameter');
-  //   }
-  //   return parsedValue;
-  // }
+  // Hàm phụ trợ để kiểm tra và phân tích các tham số
+  private parseQueryParam(value: string): number {
+    const parsedValue = parseInt(value, 10);
+    if (isNaN(parsedValue) || parsedValue < 1) {
+      throw new BadRequestException('Invalid parameter');
+    }
+    return parsedValue;
+  }
 
-  // @Get(':_id')
-  // findOne(@Param('_id') _id: string) {
-  //   return this.specialtiesService.findOne(_id);
-  // }
+  @Get(':_id')
+  findOne(@Param('_id') _id: string) {
+    return this.specialtiesService.findOne(_id);
+  }
 
-  // @Patch(':_id')
-  // update(@Param('_id') _id: string, @Body() updateSpecialtyDto: UpdateSpecialtyDto) {
-  //   return this.specialtiesService.update(_id, updateSpecialtyDto);
-  // }
+  @Patch(':_id')
+  update(
+    @Param('_id') _id: string,
+    @Body() updateSpecialtyDto: UpdateSpecialtyDto,
+  ) {
+    return this.specialtiesService.update(_id, updateSpecialtyDto);
+  }
 
-  // @Delete(':_id')
-  // remove(@Param('_id') _id: string) {
-  //   return this.specialtiesService.remove(_id);
-  // }
+  @Delete(':_id')
+  remove(@Param('_id') _id: string) {
+    return this.specialtiesService.remove(_id);
+  }
 }
