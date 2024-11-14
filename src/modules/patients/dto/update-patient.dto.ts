030c6a9cd8ec2a219052629b6c98ca42d6581efc
vietnhan1342002 <cudum13402nhan@gmail.com>
2024-11-14 11:00:03 +0700

    base schema patient

--- src/modules/patients/dto/update-patient.dto.ts
+++ src/modules/patients/dto/update-patient.dto.ts
@@ -1,4 +1,18 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePatientDto } from './create-patient.dto';
+import { IsDateString, IsEnum, IsOptional, IsString } from 'class-validator';
+import { Gender } from '../schemas/patient.schema';
 
-export class UpdatePatientDto extends PartialType(CreatePatientDto) {}
+export class UpdatePatientDto extends PartialType(CreatePatientDto) {
+  @IsOptional() // Trường này không bắt buộc
+  @IsDateString()
+  dateOfBirth?: string; // Chỉ cần cập nhật nếu muốn
+
+  @IsOptional() // Trường này không bắt buộc
+  @IsString()
+  address?: string; // Chỉ cần cập nhật nếu muốn
+
+  @IsOptional() // Trường này không bắt buộc
+  @IsEnum(Gender)
+  gender?: Gender; // Chỉ cần cập nhật nếu muốn
+}
--- src/modules/patients/schemas/patient.schema.ts
+++ src/modules/patients/schemas/patient.schema.ts
@@ -1,27 +1,22 @@
 import { MedicalRecord } from '@/modules/medical_records/schemas/medical_record.schema';
+import { UserAuth } from '@/modules/user-auth/schemas/user-auth.schema';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument, Types } from 'mongoose';
 
 export type PatientDocument = HydratedDocument<Patient>;
 
-enum Gender {
-  MALE = 'Nam',
-  FEMALE = 'Nữ',
+export enum Gender {
+  MALE = 'male',
+  FEMALE = 'female',
 }
 
 @Schema({ timestamps: true })
 export class Patient {
-  @Prop({ required: true })
-  name: string;
+  @Prop({ type: Types.ObjectId, ref: UserAuth.name, required: true })
+  userId: Types.ObjectId;
 
   @Prop({ required: true })
   dateOfBirth: Date;
-
-  @Prop({ required: true })
-  phone: string;
-
-  @Prop({ required: true })
-  email: string;
 
   @Prop({ required: true })
   address: string;
