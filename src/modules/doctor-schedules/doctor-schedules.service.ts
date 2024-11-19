66c7479a0e37f3bbed127cad5f747ebf8a9b7902
vietnhan1342002 <cudum13402nhan@gmail.com>
2024-11-19 09:47:53 +0700

    CRUD doctor-schedules

--- src/modules/doctor-schedules/doctor-schedules.service.ts
+++ src/modules/doctor-schedules/doctor-schedules.service.ts
@@ -1,26 +1,138 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateDoctorScheduleDto } from './dto/create-doctor-schedule.dto';
 import { UpdateDoctorScheduleDto } from './dto/update-doctor-schedule.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { DoctorSchedule } from './schemas/doctor-schedule.schema';
+import { Model, Types } from 'mongoose';
+import { formatDate, isExistHelper } from '@/helpers/utils';
+import aqp from 'api-query-params';
 
 @Injectable()
 export class DoctorSchedulesService {
-  create(createDoctorScheduleDto: CreateDoctorScheduleDto) {
-    return 'This action adds a new doctorSchedule';
+  constructor(
+    @InjectModel(DoctorSchedule.name)
+    private doctorScheduleModel: Model<DoctorSchedule>,
+  ) {}
+
+  private async checkDoctorScheduleExistence(
+    doctorId: string,
+    shiftId: string,
+    date: Date,
+  ) {
+    // Tìm lịch trình có cùng doctorId, shiftId và date
+    const scheduleExists = await this.doctorScheduleModel.findOne({
+      doctorId,
+      shiftId,
+      date,
+    });
+
+    // Log để kiểm tra
+    console.log('Input:', { doctorId, shiftId, date });
+    console.log('Existing Schedule:', scheduleExists);
+
+    if (scheduleExists) {
+      throw new BadRequestException(
+        `Doctor schedule for Doctor ID: ${doctorId}, Shift ID: ${shiftId}, and Date: ${date.toISOString().split('T')[0]} already exists. Please select another date/time!`,
+      );
+    }
   }
 
-  findAll() {
-    return `This action returns all doctorSchedules`;
+  async create(createDoctorScheduleDto: CreateDoctorScheduleDto) {
+    const { doctorId, shiftId, date, status } = createDoctorScheduleDto;
+
+    // Check if the schedule for this doctor on this date already exists
+    await this.checkDoctorScheduleExistence(doctorId, shiftId, date);
+
+    const schedule = await this.doctorScheduleModel.create({
+      doctorId,
+      shiftId,
+      date,
+      status,
+    });
+    return { _id: schedule.id };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} doctorSchedule`;
+  async findAll(query: string, current: number, pageSize: number) {
+    const { filter, sort } = aqp(query);
+
+    if (filter.current) delete filter.current;
+    if (filter.pageSize) delete filter.pageSize;
+
+    if (!current) current = 1;
+    if (!pageSize) pageSize = 10;
+
+    const totalItems = (await this.doctorScheduleModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / pageSize);
+
+    const skip = (current - 1) * pageSize;
+
+    // Tạo một truy vấn Mongoose và gọi populate trên đó
+    const queryResult = this.doctorScheduleModel
+      .find(filter)
+      .limit(pageSize)
+      .skip(skip)
+      .sort(sort as any);
+
+    // Thực thi truy vấn với populate
+    const result = await this.populateDoctorScheduleQuery(queryResult).exec();
+
+    if (result.length === 0)
+      throw new NotFoundException('No doctor schedules available');
+
+    return { result, totalPages };
   }
 
-  update(id: number, updateDoctorScheduleDto: UpdateDoctorScheduleDto) {
-    return `This action updates a #${id} doctorSchedule`;
+  async findOne(_id: string) {
+    const result = await this.populateDoctorScheduleQuery(
+      this.doctorScheduleModel
+        .findById({ _id })
+        .select('doctorId shiftId date status'),
+    );
+    if (!result) {
+      throw new NotFoundException(`Doctor schedule with ID ${_id} not found`);
+    }
+
+    return {
+      result,
+    };
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} doctorSchedule`;
+  async update(_id: string, updateDoctorScheduleDto: UpdateDoctorScheduleDto) {
+    const schedule = await this.findOne(_id);
+    const { doctorId, shiftId, date, status } = updateDoctorScheduleDto;
+
+    // Check if the schedule for this doctor on this date already exists
+    await this.checkDoctorScheduleExistence(doctorId, shiftId, date);
+
+    return await this.doctorScheduleModel.updateOne(
+      { _id },
+      { doctorId, shiftId, date, status },
+    );
+  }
+
+  async remove(_id: string) {
+    const schedule = await this.findOne(_id);
+    await this.doctorScheduleModel.deleteOne({ _id });
+
+    return { message: `Doctor schedule with ID ${_id} deleted successfully` };
+  }
+
+  //--------------------------/--------------------------------//
+  private populateDoctorScheduleQuery(query: any) {
+    return query
+      .populate({
+        path: 'doctorId',
+        select: 'userId ',
+
+        populate: {
+          path: 'userId',
+          select: 'fullName phoneNumber',
+        },
+      })
+      .populate({ path: 'shiftId', select: 'name' });
   }
 }
--- src/modules/doctor-schedules/dto/create-doctor-schedule.dto.ts
+++ src/modules/doctor-schedules/dto/create-doctor-schedule.dto.ts
@@ -1,1 +1,23 @@
-export class CreateDoctorScheduleDto {}
+import { Type } from 'class-transformer';
+import { IsDateString, IsEnum, IsMongoId, IsNotEmpty } from 'class-validator';
+import { Status } from '../schemas/doctor-schedule.schema';
+
+export class CreateDoctorScheduleDto {
+  @IsNotEmpty({ message: 'doctorId cannot be empty' })
+  @IsMongoId({ message: 'doctorId invalid' })
+  doctorId: string;
+
+  @IsNotEmpty({ message: 'shiftId cannot be empty' })
+  @IsMongoId({ message: 'shiftId invalid' })
+  shiftId: string;
+
+  @IsNotEmpty({ message: 'date cannot be empty' })
+  @Type(() => Date) // Chuyển đổi kiểu từ string thành Date
+  date: Date; // Ngày của lịch trình, kiểu Date
+
+  @IsNotEmpty({ message: 'status cannot be empty' })
+  @IsEnum(Status, {
+    message: 'status must be active or inactive',
+  })
+  status: Status;
+}
