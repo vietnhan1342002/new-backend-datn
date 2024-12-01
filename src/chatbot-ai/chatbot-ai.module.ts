a416957ef7b79094eb4a6cd883bf69494e8ecfe1
vietnhan1342002 <cudum13402nhan@gmail.com>
2024-12-01 20:47:20 +0700

    update chatbotAI

--- src/chatbot-ai/chatbot-ai.module.ts
+++ src/chatbot-ai/chatbot-ai.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ChatbotAiService } from './chatbot-ai.service';
-import { ChatbotAiController } from './chatbot-ai.controller';
+import { ChatBotAiController } from './chatbot-ai.controller';
 
 @Module({
-  controllers: [ChatbotAiController],
+  controllers: [ChatBotAiController],
   providers: [ChatbotAiService],
 })
 export class ChatbotAiModule {}
