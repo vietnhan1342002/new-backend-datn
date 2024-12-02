de259c9ee930d38609388f3058a4a0b59f317438
vietnhan1342002 <cudum13402nhan@gmail.com>
2024-12-02 15:40:29 +0700

    search users, specialties, appointment

--- src/modules/search/search.module.ts
+++ src/modules/search/search.module.ts
@@ -4,11 +4,15 @@ import { SearchController } from './search.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Doctor, DoctorSchema } from '../doctors/schemas/doctor.schema';
 import { UserAuth, UserAuthSchema } from '../user-auth/schemas/user-auth.schema';
+import { Specialty, SpecialtySchema } from '../specialties/schemas/specialty.schema';
+import { Appointment, AppointmentSchema } from '../appointments/schemas/appointment.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Doctor.name, schema: DoctorSchema }]),
     MongooseModule.forFeature([{ name: UserAuth.name, schema: UserAuthSchema }]),
+    MongooseModule.forFeature([{ name: Specialty.name, schema: SpecialtySchema }]),
+    MongooseModule.forFeature([{ name: Appointment.name, schema: AppointmentSchema },]),
   ],
   controllers: [SearchController],
   providers: [SearchService],
