9c15d1ccfc4fd3553763337b016b840fef8a51ca
vietnhan1342002 <cudum13402nhan@gmail.com>
2024-12-09 14:23:27 +0700

    create patient from chatbot

--- src/chatbot-ai/chatbot-ai.module.ts
+++ src/chatbot-ai/chatbot-ai.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ChatbotAiService } from './chatbot-ai.service';
 import { ChatBotAiController } from './chatbot-ai.controller';
+import { UserAuthModule } from '@/modules/user-auth/user-auth.module';
 
 @Module({
+  imports: [
+    UserAuthModule,
+  ],
   controllers: [ChatBotAiController],
   providers: [ChatbotAiService],
 })
-export class ChatbotAiModule {}
+export class ChatbotAiModule { }
