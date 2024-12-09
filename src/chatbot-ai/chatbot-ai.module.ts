2e6ddcba324fc5e986325ee8749d8a73cadd68a0
vietnhan1342002 <cudum13402nhan@gmail.com>
2024-12-09 18:11:37 +0700

    get specialtyId and date

--- src/chatbot-ai/chatbot-ai.module.ts
+++ src/chatbot-ai/chatbot-ai.module.ts
@@ -3,11 +3,13 @@ import { ChatbotAiService } from './chatbot-ai.service';
 import { ChatBotAiController } from './chatbot-ai.controller';
 import { UserAuthModule } from '@/modules/user-auth/user-auth.module';
 import { SpecialtiesModule } from '@/modules/specialties/specialties.module';
+import { FilterModule } from '@/modules/filter/filter.module';
 
 @Module({
   imports: [
     UserAuthModule,
-    SpecialtiesModule
+    SpecialtiesModule,
+    FilterModule
   ],
   controllers: [ChatBotAiController],
   providers: [ChatbotAiService],
